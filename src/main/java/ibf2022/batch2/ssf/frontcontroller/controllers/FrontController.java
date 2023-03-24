6bca3f31fa9aadb591cc51421254242f132ccf36
stanleyseow173 <seowszehowestanley@gmail.com>
2023-03-24 09:09:07 +0800

    first commit

---
+++ src/main/java/ibf2022/batch2/ssf/frontcontroller/controllers/FrontController.java
@@ -0,0 +1,7 @@
+package ibf2022.batch2.ssf.frontcontroller.controllers;
+
+public class FrontController {
+
+	// TODO: Task 2, Task 3, Task 4, Task 6
+	
+}
---
+++ src/main/java/ibf2022/batch2/ssf/frontcontroller/respositories/AuthenticationRepository.java
@@ -0,0 +1,8 @@
+package ibf2022.batch2.ssf.frontcontroller.respositories;
+
+public class AuthenticationRepository {
+
+	// TODO Task 5
+	// Use this class to implement CRUD operations on Redis
+
+}
